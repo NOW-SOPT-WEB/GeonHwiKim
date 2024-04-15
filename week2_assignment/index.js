1b71ecc4e1c5f1f7933d2e8ee42a76ff2f7c8b28
KIMGEONHWI <dirnrlarjsgn@naver.com>
2024-04-15 22:04:01 +0900

    feat: 새브렌치 연결

---
+++ week2_assignment/index.js
@@ -0,0 +1,9 @@
+// 오른쪽 네비게이션 메뉴 열기
+function openNav() {
+    document.getElementById("rightNav").style.width = "250px";
+}
+
+// 오른쪽 네비게이션 메뉴 닫기
+function closeNav() {
+    document.getElementById("rightNav").style.width = "0";
+}
