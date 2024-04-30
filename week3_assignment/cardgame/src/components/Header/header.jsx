76d9997eb61b727ea0ec01b804fcc4d9f966cfc3
KIMGEONHWI <dirnrlarjsgn@naver.com>
2024-05-01 03:37:34 +0900

    feat: Reset버튼 추가 및 기능 구현

--- week3_assignment/cardgame/src/components/Header/header.jsx
+++ week3_assignment/cardgame/src/components/Header/header.jsx
@@ -1,10 +1,17 @@
 import styled from "@emotion/styled";
 
 function MainHeader({ numPairs, matchedPairsCount }) {
+  const handleReset = () => {
+    window.location.reload();
+  };
+
   return (
     <MainHeaderWrapper>
-      <MainHeaderTitle>옷 맞추기</MainHeaderTitle>
-      <ScoreDisplay>{matchedPairsCount}/{numPairs}</ScoreDisplay>
+      <MainHeaderContent>
+        <MainHeaderTitle>옷 맞추기</MainHeaderTitle>
+        <ScoreDisplay>{matchedPairsCount}/{numPairs}</ScoreDisplay>
+      </MainHeaderContent>
+      <ResetButton onClick={handleReset}>Reset</ResetButton>
     </MainHeaderWrapper>
   );
 }
@@ -13,24 +20,37 @@ export default MainHeader;
 
 const MainHeaderWrapper = styled.section`
   display: flex;
-  flex-direction: column;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
-  row-gap: 3rem;
-
-  width: 100%;
-  height: 20rem;
-
-  color: ${({ theme }) => theme.colors.black};
-
+  padding: 0 2rem;
+  height: 10rem;
   background-color: ${({ theme }) => theme.colors.secondmint};
+`;
+
+const MainHeaderContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin-left: 77rem;
 `;
 
 const MainHeaderTitle = styled.h1`
   font-size: ${({ theme }) => theme.fonts.xl};
+  margin-right: 2rem;
 `;
 
 const ScoreDisplay = styled.span`
   font-size: ${({ theme }) => theme.fonts.xl};
-  color: ${({ theme }) => theme.colors.black};
+`;
+
+const ResetButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: ${({ theme }) => theme.fonts.large};
+  padding: 3rem 6rem;
+  background-color: ${({ theme }) => theme.colors.firstmint};
+  border: none;
+  border-radius: 5px;
 `;
