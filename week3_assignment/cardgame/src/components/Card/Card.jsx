e93b4203b69c83c3e8b988df1667206bb2e6d346
KIMGEONHWI <dirnrlarjsgn@naver.com>
2024-04-30 15:04:52 +0900

    feat: 화면 랜더링 될 때마다 카드 이미지 랜덤하게 섞어주기

--- week3_assignment/cardgame/src/components/Card/Card.jsx
+++ week3_assignment/cardgame/src/components/Card/Card.jsx
@@ -2,7 +2,18 @@ import styled from "@emotion/styled";
 import { CARD_LIST } from "../../constants/cardlist";
 
 const Card = () => {
-  return CARD_LIST.map((card, index) => (
+  // 배열을 섞는 함수 정의
+  const shuffleArray = (array) => {
+    return array
+      .map((value) => ({ value, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(({ value }) => value);
+  };
+
+  // CARD_LIST 배열을 랜덤하게 섞기
+  const shuffledCards = shuffleArray(CARD_LIST);
+
+  return shuffledCards.map((card, index) => (
     <CardWrapper key={index}>
       <img src={card.imgSrc} alt={card.imgAlt} />
     </CardWrapper>
