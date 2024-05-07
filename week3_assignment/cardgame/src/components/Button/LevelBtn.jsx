fce814e7303a422993f08a22f2d80b9d0c6b6e2b
KIMGEONHWI <dirnrlarjsgn@naver.com>
2024-05-07 20:28:19 +0900

    style: level 버튼 css속성 순서 조정

--- week3_assignment/cardgame/src/components/Button/LevelBtn.jsx
+++ week3_assignment/cardgame/src/components/Button/LevelBtn.jsx
@@ -11,13 +11,13 @@ const LevelBtn = ({ level, onClick, selected }) => {
 export default LevelBtn;
 
 const Level = styled.button`
-    padding: 3rem 6rem;
-    border: 0.1rem solid;
-    border-radius: 1rem;
-    font-weight: bold;
     display: flex;
     justify-content: center;
     align-items: center;
+    padding: 3rem 6rem;
+    border: 0.1rem solid;
+    border-radius: 1rem;
     background-color: ${props => props.selected ? props.theme.colors.firstmint : props.theme.colors.white};
     color: ${props => props.selected ? props.theme.colors.white : props.theme.colors.black};
+    font-weight: bold;
 `;
