17df3d9893fa99e671d40e9168961fd258b4bf01
KIMGEONHWI <dirnrlarjsgn@naver.com>
2024-05-09 00:34:19 +0900

    Refactor: .prettierrc 파일 수정

--- week4_assignment/login-signup-project/src/pages/Login.jsx
+++ week4_assignment/login-signup-project/src/pages/Login.jsx
@@ -1,18 +1,17 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 function Login() {
   return (
     <LoginPageContainer>
       <LoginBoxContainer>
         <LoginTitle>Login</LoginTitle>
-        <LoginImg src="/src/assets/loginmainimg.png" />
+        <LoginImg src='/src/assets/loginmainimg.png' />
         <InputSection>
           <InputContainer>
             <IdPwTitle>ID</IdPwTitle>
             <InputBox />
           </InputContainer>
           <InputContainer>
-            <IdPwTitle>PW</IdPwTitle>
             <InputBox />
           </InputContainer>
         </InputSection>
@@ -34,7 +33,7 @@ const LoginPageContainer = styled.main`
   width: 100%;
   height: 100vh;
   background-color: aliceblue;
-`
+`;
 
 const LoginBoxContainer = styled.section`
   display: flex;
@@ -45,17 +44,17 @@ const LoginBoxContainer = styled.section`
   height: 50rem;
   padding: 5rem 7rem;
   background-color: white;
-`
+`;
 
 const LoginTitle = styled.h1`
   font-size: 3rem;
-`
+`;
 
 const LoginImg = styled.img`
   width: 12rem;
   height: 12rem;
   margin-top: 3rem;
-`
+`;
 
 const InputSection = styled.section`
   display: flex;
@@ -63,7 +62,7 @@ const InputSection = styled.section`
   gap: 2rem;
 
   width: 100%;
-`
+`;
 
 const InputContainer = styled.div`
   display: flex;
@@ -72,30 +71,30 @@ const InputContainer = styled.div`
   width: 100%;
 
   margin-top: 3rem;
-`
+`;
 
 const IdPwTitle = styled.p`
   font-size: 3rem;
-`
+`;
 
 const InputBox = styled.input`
   width: 80%;
   height: 4rem;
   padding-left: 1rem;
-`
+`;
 
 const BtnSection = styled.section`
   display: flex;
 
   gap: 3rem;
   margin-top: 3rem;
-`
+`;
 
 const Button = styled.button`
   width: 10rem;
   height: 5rem;
 
   border-radius: 1rem;
-  
+
   background-color: aliceblue;
-`
+`;
