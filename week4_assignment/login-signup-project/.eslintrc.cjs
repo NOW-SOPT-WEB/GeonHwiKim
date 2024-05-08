0d13b434e5aad2531ad33b7831f3f80de165ee41
KIMGEONHWI <dirnrlarjsgn@naver.com>
2024-05-08 15:44:41 +0900

    Chore: eslint와prettier 함께 사용 적용

--- week4_assignment/login-signup-project/.eslintrc.cjs
+++ week4_assignment/login-signup-project/.eslintrc.cjs
@@ -6,19 +6,18 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended', // Prettier 추천 설정 추가
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'], // Prettier 플러그인 추가
   rules: {
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
-  },
-  "rules": {
     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
     "react/react-in-jsx-scope": "off",
     "no-console": "warn",
@@ -35,5 +34,6 @@ module.exports = {
       "destructuring": "any",
       "ignoreReadBeforeAssign": false
     }],
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }], // Prettier 규칙 사용
   },
 }
