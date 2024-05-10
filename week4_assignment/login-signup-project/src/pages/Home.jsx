d73032968847eca799f914e1d2028d1fc4674436
KIMGEONHWI <dirnrlarjsgn@naver.com>
2024-05-10 21:14:01 +0900

    feat: Home  페이지 구현 및 api연결

--- week4_assignment/login-signup-project/src/pages/Home.jsx
+++ week4_assignment/login-signup-project/src/pages/Home.jsx
@@ -1,5 +1,59 @@
+import styled from 'styled-components';
+import { useNavigate, useParams } from 'react-router-dom';
+
 function Home() {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+  const { memberId } = useParams();
+
+  const handleMyPageClick = () => {
+    navigate(`/mypage/${memberId}`);
+  }
+  
+
+  const handleJoinClick = () => {
+    navigate('/signup');
+  };
+
+
+
+  return (
+    <HomePageContainer>
+      <HomeImg src='/src/assets/Homeimg.jpeg' />
+      <HomeBtnSection>
+        <HomePageBtn onClick={handleMyPageClick}>내 정보</HomePageBtn>
+        <HomePageBtn onClick={handleJoinClick}>회원가입</HomePageBtn>
+      </HomeBtnSection>
+    </HomePageContainer>
+  )
 }
 
 export default Home;
+
+const HomePageContainer = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+  background-color: aliceblue;
+`
+
+const HomeImg = styled.img`
+  width: 60rem;
+  height: 30rem;
+  margin-top: 3rem;
+`
+
+const HomeBtnSection = styled.section`
+  display: flex;
+  gap: 3rem;
+  margin-top: 10rem;
+`
+
+const HomePageBtn = styled.button`
+  width: 10rem;
+  height: 5rem;
+  border-radius: 1rem;
+  background-color: green;
+`
